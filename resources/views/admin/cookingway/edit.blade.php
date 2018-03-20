d692bf4da113021e35b251b6de54aea7efba8e36
xwamy <346271553@qq.com>
2018-03-20 10:02:42 +0800

    第一次提交代码

---
+++ resources/views/admin/cookingway/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('admin.layouts.admin')
+
+@section('admin-css')
+    <link href="{{ asset('asset_admin/assets/plugins/parsley/src/parsley.css') }}" rel="stylesheet" />
+    <link href="{{ asset('asset_admin/assets/plugins/bootstrap-select/bootstrap-select.min.css') }}" rel="stylesheet" />
+@endsection
+
+@section('admin-content')
+    <div id="content" class="content">
+        <!-- begin breadcrumb -->
+        <ol class="breadcrumb pull-right">
+            <li><a href="javascript:;">主页</a></li>
+            <li><a href="javascript:;">烹饪方式列表</a></li>
+            <li class="active">编辑菜单</li>
+        </ol>
+        <!-- end breadcrumb -->
+        <!-- begin page-header -->
+        <h1 class="page-header">修改烹饪方式</h1>
+        <!-- end page-header -->
+
+        <!-- begin row -->
+        <div class="row">
+            <!-- begin col-6 -->
+            <div class="col-md-12">
+                <!-- begin panel -->
+                <div class="panel panel-inverse" data-sortable-id="form-validation-1">
+                    <div class="panel-heading">
+                        <div class="panel-heading-btn">
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                        </div>
+                        <h4 class="panel-title">表单</h4>
+                    </div>
+                    @if(count($errors)>0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <div class="panel-body panel-form">
+                        <form class="form-horizontal form-bordered" data-parsley-validate="true" action="{{ url('admin/cookingway/'.$cookingway['id']) }}" method="post">
+                            {{ csrf_field() }}
+                            {{ method_field('patch') }}
+                            <div class="form-group">
+                                <label class="control-label col-md-4 col-sm-4" for="name">烹饪方式名称 * :</label>
+                                <div class="col-md-6 col-sm-6">
+                                    <input class="form-control" type="text" name="name" placeholder="烹饪方式名称" data-parsley-required="true" data-parsley-required-message="请输入烹饪方式名称"  value="{{ $cookingway['name'] }}"/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-4 col-sm-4" for="url">排序（升序） * :</label>
+                                <div class="col-md-6 col-sm-6">
+                                    <input class="form-control" type="text" name="sort" placeholder="排序" data-parsley-required="true" data-parsley-type="integer" data-parsley-required-message="请输入排序"  value="{{ $cookingway['sort'] }}" />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-4 col-sm-4"></label>
+                                <div class="col-md-6 col-sm-6">
+                                    <button type="submit" class="btn btn-primary">提交</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end panel -->
+            </div>
+            <!-- end col-6 -->
+        </div>
+        <!-- end row -->
+    </div>
+@endsection
+
+@section('admin-js')
+    <script src="{{ asset('asset_admin/assets/plugins/parsley/dist/parsley.js') }}"></script>
+    <script src="{{ asset('asset_admin/assets/plugins/bootstrap-select/bootstrap-select.min.js') }}"></script>
+    <script>
+        $('.selectpicker').selectpicker('render');
+    </script>
+@endsection
